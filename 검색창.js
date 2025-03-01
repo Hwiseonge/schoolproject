1466764742c181b0f74f0c0bc18f65df50d19368
xhaktn <lsynoel1225@gmail.com>
2025-03-02 05:40:42 +0900

    Update 검색창.js

--- 검색창.js
+++ 검색창.js
@@ -38,16 +38,17 @@ function showSearchResults(inputId, resultId, data, category) {
     let similarMatches = [];
 
     for (let floor in floors) {
-    floors[floor].rooms.forEach(room => {
-        if (room.includes(searchInput)) {
-            exactMatch = `<p><strong>${room}</strong>: ${floor}에 위치</p>`;
+        if (floors[floor].rooms.includes(searchInput)) {
             foundFloor = floor;
-        } 
-        else if (searchInput.length > 1 && (room.includes(searchInput) || searchInput.includes(room))) { 
-            similarMatches.push(`<p><strong>${room}</strong>: ${floor}에 위치</p>`);
+            exactMatch = `<p><strong>${searchInput}</strong>: ${floor}에 위치</p>`;
+        } else {
+            floors[floor].rooms.forEach(room => {
+                if (room.includes(searchInput)) {
+                    similarMatches.push(`<p><strong>${room}</strong>: ${floor}에 위치</p>`);
+                }
+            });
         }
-    });
-}
+    }
 
     console.log(`검색어: ${searchInput}, 찾은 층: ${foundFloor}, 카테고리 : ${category}`);
 
